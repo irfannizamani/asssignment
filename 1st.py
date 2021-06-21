a7ee45b3db22893940c8201093d64cc7e8c16967
Abdul-Ahad9 <38209766+Abdul-Ahad9@users.noreply.github.com>
2021-06-21 16:04:05 +0500

    Update 1st.py

--- 1st.py
+++ 1st.py
@@ -1,4 +1,4 @@
-"""
+print('this is forked code by another user')
 
 print('I will guess whom are you thinking about!')
 print('Note: he must be the resident of Tando-Qaiser.')
@@ -9,9 +9,4 @@ if value == 'Yes':
         value2 = input('Is ')
 elif value == 'No':
     print('Bye')
-"""
-f = int(input("enter"))
-for x in range(2,f):
-    print(10**x)
-
 
