dd9be53d26e7de38c7a9ea40427ad8374368f2c1
irfannizamani <76012153+irfannizamani@users.noreply.github.com>
2021-06-21 15:20:54 +0500

    Add files via upload

---
+++ 1st.py
@@ -0,0 +1,17 @@
+"""
+
+print('I will guess whom are you thinking about!')
+print('Note: he must be the resident of Tando-Qaiser.')
+value = input('Is he Nizamani?')
+if value == 'Yes':
+    value1 = input('Type his Mohalla:')
+    if value1 != ("Qaisrani", "Qazi", "Dakhani", "Bakhrani", "Allodani", "Shafi Colony"):
+        value2 = input('Is ')
+elif value == 'No':
+    print('Bye')
+"""
+f = int(input("enter"))
+for x in range(2,f):
+    print(10**x)
+
+
